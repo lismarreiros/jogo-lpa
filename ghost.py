b05c09391a2a829784f598f27116f33f2988ed89
lismarreiros <lismarreiros@hotmail.com>
2025-10-26 13:04:11 -0300

    Add Ghost character and update game logic; remove unused assets.

--- ghost.py
+++ ghost.py
@@ -1,41 +1,23 @@
 import pygame
 
-class Reaper:
-  def __init__(self, imagem, frame_size, pos_inicial, velocidade=5, vida=100):
+class Ghost:
+  def __init__(self, imagem, frame_size, posicao_inicial, velocidade=15, vida=100):
     self.imagem = imagem
     self.frame_size = frame_size
     self.anim_frame = 0
     self.anim_speed = 0.1
     self.direcao = "idle"
-
-    self.x, self.y = pos_inicial
+    self.x, self.y = posicao_inicial
+    self.x_inicial = posicao_inicial[0]
     self.velocidade = velocidade
-
+    # controla a direção do movimento: 1 para direita, -1 para esquerda
+    self.direcao_movimento = 1
+ 
     self.vida = vida
     self.vivo = True
     self.forca = 10
 
     self.estado = "idle"
-    self.cooldown_ataque = 500
-    self.tempo_desde_ataque = 0
-
-  def mover(self, direcao):
-    if direcao == "left":
-      self.x -= self.velocidade
-    elif direcao == "right":
-      self.x += self.velocidade
-    elif direcao == "up":
-      self.y -= self.velocidade
-    elif direcao == "down":
-      self.y += self.velocidade
-    self.direcao = direcao
-    self.estado = "moving"
-
-  def atacar(self):
-    if self.tempo_desde_ataque >= self.cooldown_ataque:
-      print("Reaper atacou!")
-      self.estado = "attacking"
-      self.tempo_desde_ataque = 0
 
   def levar_dano(self, dano):
     self.vida -= dano
@@ -46,17 +28,28 @@ class Reaper:
         self.estado = "hit"
 
   def atualizar(self, delta_time):
-    self.tempo_desde_ataque += delta_time
     total_frames = self.imagem.get_width() // self.frame_size[0]
     self.anim_frame += self.anim_speed
     if self.anim_frame >= total_frames:
       self.anim_frame = 0
+
+    movimento = self.velocidade * self.direcao_movimento * (delta_time / 1000)
+    self.x += movimento 
+    
+    # se bateu na borda direita da tela, inverte a direção
+    if self.x > 690:
+      self.x = 690
+      self.direcao_movimento = -1  
+    elif self.x <= self.x_inicial:
+      self.x = self.x_inicial
+      self.direcao_movimento = 1
+
 
   def get_frame(self, frame_index):
     frame_width, frame_height = self.frame_size
     rect = pygame.Rect(frame_index * frame_width, 0, frame_width, frame_height)
     return self.imagem.subsurface(rect)
 
-  def desenhar(self, tela, scale = 2):
+  def desenhar(self, tela, scale = 1.5):
     frame = pygame.transform.scale(self.get_frame(int(self.anim_frame)), (self.frame_size[0]*scale, self.frame_size[1]*scale))
     tela.blit(frame, (self.x, self.y))
--- jogo.py
+++ jogo.py
@@ -1,20 +1,33 @@
 import pygame
 import sys
 import reaper  
-
+import ghost
 
 pygame.init()  # inicializa os modulos do pygame
 clock = pygame.time.Clock()  # cria um relogio para controlar o fps
+test_font = pygame.font.Font('fonts/Pixeltype.ttf', 50) 
 # cenário
-imagem_mapa = pygame.image.load("./imagens/mapa.png")  # carrega uma imagem em url relativa
+imagem_mapa = pygame.image.load("./imagens/mapa.png")
+menuzinho = pygame.Surface((200,100))
+menuzinho.fill((3, 22, 28))
+
+# origin point (0,0) is top-left
 tela = pygame.display.set_mode(size=(700, 500))  # cria uma tela com largura, altura
 imagem_mapa = pygame.transform.scale(imagem_mapa, (700, 500))  # redimensiona a imagem
+text_surface = test_font.render("Tanatos", True, (43, 100, 113)) 
 
-# reaper 
-imagem_reaper = pygame.image.load("./imagens/reaper/PassiveIdleReaper-Sheet.png").convert_alpha()  # carrega uma imagem em url relativa
+
+# reaper - player
+imagem_reaper = pygame.image.load("./imagens/reaper/HostileIdleReaper-Sheet.png").convert_alpha() 
 posicao_inicial_reaper = (100, 325)
 frame_size_reaper = (48, 48)
 reaper1 = reaper.Reaper(imagem_reaper, frame_size_reaper, posicao_inicial_reaper)
+
+# ghost - enemy
+imagem_ghost = pygame.image.load("./imagens/ghost/ghost-idle.png").convert_alpha()
+posicao_inicial_ghost = (400, 348)
+frame_size_ghost = (32, 32)
+ghost1 = ghost.Ghost(imagem_ghost, frame_size_ghost, posicao_inicial_ghost)
 
 
 som_espada = pygame.mixer.Sound("./sons/espada.mp3")  # carrega um som, pode ser wav, ogg, mp3 etc.
@@ -32,9 +45,13 @@ while True:
 
 
     # desenha elementos na tela e atualiza ela
-    tela.blit(imagem_mapa, (0, 0)) 
+    tela.blit(imagem_mapa, (0, 0))
+    tela.blit(menuzinho, (10, 10))
+    tela.blit(text_surface, (10, 10))
     reaper1.atualizar(delta_time)
     reaper1.desenhar(tela)
+    ghost1.atualizar(delta_time)
+    ghost1.desenhar(tela)
 
     # if cacador_vida > 0:
     #     tela.blit(imagem_cacador, (cacador_x, cacador_y))  # desenha a imagem na tela na posicao x, y
